d9e11733a51bdfba026c62d188799aa14faf1136
vFlicker <flickervladislav@gmail.com>
2022-06-21 19:38:29 +0300

    chore: set up the project

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+// import Jimp from 'jimp';
+// import robot from 'robotjs';
+// import { WebSocketServer } from 'ws';
+
+import { httpServer } from './http-server'
+
+const HTTP_PORT = 3000
+
+console.log(`Start static http server on the ${HTTP_PORT} port!`)
+httpServer.listen(HTTP_PORT)

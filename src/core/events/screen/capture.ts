e1402334761a2b447db6d799ac7945cbacb8071b
vFlicker <flickervladislav@gmail.com>
2022-06-24 22:41:29 +0300

    feat: add screen event

--- src/core/events/screen/capture.ts
+++ src/core/events/screen/capture.ts
@@ -1,6 +1,22 @@
-// import Jimp from 'jimp';
+import robot from 'robotjs'
 import { WebSocket } from 'ws'
 
-export const capture = (ws: WebSocket) => {
-  ws.send('prnt_scrn')
+import { base64Image } from './utils'
+
+const WIDTH = 200
+const HEIGHT = 200
+const DUPLICATE_LINE = 'data:image/png;base64,'
+
+export const capture = async (ws: WebSocket) => {
+  const mouse = robot.getMousePos()
+  const x = mouse.x - 100
+  const y = mouse.y - 100
+  const bitmap = robot.screen.capture(x, y, WIDTH, HEIGHT)
+
+  try {
+    const base64 = await base64Image(bitmap.image, WIDTH, HEIGHT)
+    ws.send(`prnt_scrn ${base64.replace(DUPLICATE_LINE, '')}`)
+  } catch (err) {
+    console.log(err)
+  }
 }
---
+++ src/core/events/screen/utils.ts
@@ -0,0 +1,28 @@
+import Jimp from 'jimp'
+
+export const base64Image = async (
+  buffer: Buffer,
+  width: number,
+  height: number,
+) => {
+  const jimp = new Jimp(width, height)
+  let red: number, green: number, blue: number
+  buffer.forEach((byte, i) => {
+    switch (i % 4) {
+      case 0:
+        return (blue = byte)
+      case 1:
+        return (green = byte)
+      case 2:
+        return (red = byte)
+      case 3:
+        jimp.bitmap.data[i - 3] = red
+        jimp.bitmap.data[i - 2] = green
+        jimp.bitmap.data[i - 1] = blue
+        jimp.bitmap.data[i] = 255
+    }
+  })
+
+  const result = await jimp.getBase64Async(Jimp.MIME_PNG)
+  return result
+}

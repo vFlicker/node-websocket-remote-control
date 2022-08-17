81c1439140a4f9d025bc80baa5cef2e0668c3f6c
Vladislav <flickervladislav@gmail.com>
2022-08-17 20:13:29 +0300

    refactor: main file

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,46 @@ import WebSocket, { createWebSocketStream, WebSocketServer } from 'ws';
 import { httpServer } from './http-server';
 import { parseMessage, write } from './utils';
 import * as commands from './commands';
+import internal from 'stream';
 
 const PORT = 3000;
 
 const wss = new WebSocketServer({ server: httpServer });
 
-const onHttpServerConnectionHandler = () => {
-  write(`Start static http server on the ${PORT} port!`);
+const onMessageHandler = async (
+  chunk: WebSocket.RawData,
+  duplex: internal.Duplex,
+) => {
+  const { type, payload } = parseMessage(chunk);
+  const command = commands[type];
+
+  if (!command) {
+    const answer = `Unknown action: ${type}`;
+    write(answer, 'error');
+    duplex.write(`${answer}\0`);
+    return;
+  }
+
+  write(
+    `Received: command ${type} ${
+      payload.length ? `with payload ${payload}` : ''
+    }`,
+    'info',
+  );
+
+  try {
+    const result = await command(payload);
+    result ? duplex.write(`${result}\0`) : duplex.write(`${type}\0`);
+
+    write(
+      `Command ${type} ${result ? `${result} executed successfully` : ''}`,
+      'success',
+    );
+  } catch (err) {
+    const answer = `Command ${type} failed with error`;
+    write(answer, 'error');
+    duplex.write(`${answer}\0`);
+  }
 };
 
 const onWebSocketConnectionHandler = (ws: WebSocket) => {
@@ -18,58 +51,20 @@ const onWebSocketConnectionHandler = (ws: WebSocket) => {
     decodeStrings: false,
   });
 
-  const onMessageHandler = async (chunk: WebSocket.RawData) => {
-    const action = parseMessage(chunk);
-    const command = commands[action.type];
-
-    if (!command) {
-      const answer = `Unknown action: ${action.type}`;
-      write(answer, 'error');
-      duplex.write(`${answer}\0`);
-      return;
-    }
-
-    write(
-      `Received: command ${action.type} ${
-        action.payload.length ? `with payload ${action.payload}` : ''
-      }`,
-      'info',
-    );
-
-    try {
-      const result = await command(action.payload);
-      result ? duplex.write(`${result}\0`) : duplex.write(`${action.type}\0`);
-
-      write(
-        `Command ${action.type} ${
-          result ? `${result} executed successfully` : ''
-        }`,
-        'success',
-      );
-    } catch (err) {
-      const answer = `Command ${action.type} failed with error`;
-      write(answer, 'error');
-      duplex.write(`${answer}\0`);
-    }
-  };
-
-  const onErrorHandler = () => duplex.write('Something went wrong\0');
-
-  const onCloseHandler = () => write('User disconnected');
-
-  const onWebSocketCloseHandler = () => {
-    duplex.end();
-    write('Websocket server closed');
-  };
-
   write(`Start websocket server on the ${PORT} port!`);
 
-  duplex.on('data', (chunk) => onMessageHandler(chunk));
-  duplex.on('error', onErrorHandler);
-  duplex.on('close', onCloseHandler);
+  duplex.on('data', (chunk) => onMessageHandler(chunk, duplex));
+  duplex.on('error', () => duplex.write('Something went wrong\0'));
+  duplex.on('close', () => write('User disconnected'));
 
-  wss.on('close', onWebSocketCloseHandler);
+  wss.on('close', () => {
+    duplex.end();
+    write('Websocket server closed');
+  });
 };
 
-httpServer.listen(PORT, onHttpServerConnectionHandler);
+httpServer.listen(PORT, () => {
+  write(`Start static http server on the ${PORT} port!`);
+});
+
 wss.on('connection', onWebSocketConnectionHandler);

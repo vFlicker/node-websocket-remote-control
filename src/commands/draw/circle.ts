18cefd0494b41e2a253b97170fed7c5e8be2ed6d
Vladislav <flickervladislav@gmail.com>
2022-08-19 17:55:15 +0300

    refactor: circle command

--- src/commands/draw/circle.ts
+++ src/commands/draw/circle.ts
@@ -5,11 +5,13 @@ export const circle = (type: string, radius: number): string => {
 
   robot.moveMouse(mousePos.x + radius, mousePos.y);
   robot.mouseToggle('down');
+
   for (let i = 0; i <= Math.PI * 2; i += 0.01) {
     const x = mousePos.x + radius * Math.cos(i);
     const y = mousePos.y + radius * Math.sin(i);
     robot.dragMouse(x, y);
   }
+
   robot.mouseToggle('up');
 
   return `${type} executed successfully`;
